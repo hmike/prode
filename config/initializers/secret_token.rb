287e7a572fafa59383256c005433eb44e3c255af
Miguel Hoffmann <hmikes@gmail.com>
2014-02-17 20:21:54 -0300

    Initial commit + facebook and google login

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prode::Application.config.secret_key_base = '7133e5db82bac9bed1e95ed9e13d7f3f708cc73c0302fdbc5a4c5e70dcabec8fa4f43bda36d2fc97eaac0c5afd3251115d16924451d3b483eb781c84a625d1f9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Prode::Application.config.session_store :cookie_store, key: '_prode_session'
