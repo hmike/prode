a92a69e8cc648aad59b4e416d71dc1cbfe2187a0
Miguel Hofmann <hmikes@gmail.com>
2014-02-26 20:35:08 -0300

    Added yeti files to production compilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Prode::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( yeti.css yeti.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
