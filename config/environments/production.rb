0c91e049659e2c905a0edbd0d171870347ea5eca
Miguel Hofmann <hmikes@gmail.com>
2014-02-26 21:39:24 -0300

    Added compile assets for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Prode::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -61,7 +61,8 @@ Prode::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( yeti.css yeti.js )
+  # config.assets.precompile += %w( yeti.css yeti.js )
+  config.assets.precompile = ['*.js', '*.css']
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
