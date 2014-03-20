a61d75f9cf9b5d07a81593d971b5f4849d0721c9
Miguel Hofmann <hmikes@gmail.com>
2014-03-19 22:32:19 -0300

    Added auth for User views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+  before_filter :authenticate_user!#, :except => [:some_action_without_auth]
+
   # GET /users
   # GET /users.json
   def index
