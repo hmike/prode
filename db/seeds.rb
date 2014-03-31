c942edff26238ce627a219dcdb66c5a158ab33f5
Miguel Hofmann <hmikes@gmail.com>
2014-03-31 19:45:41 -0300

    Added seed for Admin User

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# admin = User.where(:email => 'admin@admin.com').any?
+puts "\n\n\n"
+puts "> Admin user seed:"
+if (!User.where(:email => 'admin@admin.com').any?)
+	puts ">> Not found Admin User, creating a new one..."
+	admin = User.create(
+					:email => 'admin@admin.com',
+					:password => '123456',
+					:password_confirmation => '123456',
+					:admin => true,
+				)
+	puts ">>> Done!"
+else
+	puts ">> Already exist an Admin User, skipping seed."
+end
+puts "\n\n\n"
